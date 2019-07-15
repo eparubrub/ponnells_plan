8d7011a0309e9c5b77db95e7a56fa747f1e03741
emmit-parubrub <eparubrub@dons.usfca.edu>
2019-07-14 22:03:34 -0700

    Updates

---
+++ precache-manifest.e9d59de354cd8633704d455482b5b39d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc982cf9cf42162bf56ec3860c91fe8b",
+    "url": "/ponells_plan/index.html"
+  },
+  {
+    "revision": "7d62002ea4873228b552",
+    "url": "/ponells_plan/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ponells_plan/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7d62002ea4873228b552",
+    "url": "/ponells_plan/static/js/main.d1349325.chunk.js"
+  },
+  {
+    "revision": "70a1857a8add1159d46d",
+    "url": "/ponells_plan/static/js/runtime~main.c4b42980.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ponells_plan/static/media/logo.5d5d9eef.svg"
+  }
+]);
