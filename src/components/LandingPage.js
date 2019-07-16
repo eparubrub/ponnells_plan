506e525d14908ad911d7051202703cd96cae048a
emmit-parubrub <eparubrub@dons.usfca.edu>
2019-07-15 19:56:59 -0700

    test

---
+++ src/components/LandingPage.js
@@ -0,0 +1,16 @@
+class HomePage extends Component {
+    render() {
+      return (
+        <div className="home-body">
+
+            <h1>Hellow</h1>
+        </div>
+      );
+    }
+  }
+
+  HomePage.defaultProps = {
+  };
+  
+export default HomePage;
+  
