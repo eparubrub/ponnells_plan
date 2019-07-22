5be8c338189b9e730bd82eceab2941439825e8ec
emmit-parubrub <eparubrub@dons.usfca.edu>
2019-07-21 22:24:17 -0700

    sizing testing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ class Navbar extends Component {
     componentDidMount(){
         window.addEventListener('scroll', () => {
             let activeClass = 'normal';
-            if(window.scrollY > 550){
+            if(window.scrollY > 400){
                 activeClass = 'background';
             }
             this.setState({ activeClass });
