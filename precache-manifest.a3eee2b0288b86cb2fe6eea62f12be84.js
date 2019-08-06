42b4c8fdb3f10b0c01785f4ffe0e0298a254383b
emmit-parubrub <eparubrub@dons.usfca.edu>
2019-08-05 20:29:45 -0700

    Updates

--- precache-manifest.a3eee2b0288b86cb2fe6eea62f12be84.js
+++ precache-manifest.a3eee2b0288b86cb2fe6eea62f12be84.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f3c6e17921b25c4500010da1277f388d",
+    "revision": "b3433c14e5c8beaa24637070d8f670a2",
     "url": "/ponnells_plan/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ponnells_plan/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "255193bd74d1c989d239",
-    "url": "/ponnells_plan/static/css/main.f784878c.chunk.css"
+    "revision": "3f33d3b5d17a740fb01f",
+    "url": "/ponnells_plan/static/css/main.d4b7cd59.chunk.css"
   },
   {
     "revision": "23a8f19767cc2dd98638",
     "url": "/ponnells_plan/static/js/2.034a8bb8.chunk.js"
   },
   {
-    "revision": "255193bd74d1c989d239",
+    "revision": "3f33d3b5d17a740fb01f",
     "url": "/ponnells_plan/static/js/main.6bbe7aeb.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ponnells_plan/precache-manifest.8aa4516bea316181893fa6f7117b97cf.js"
+  "/ponnells_plan/precache-manifest.a3eee2b0288b86cb2fe6eea62f12be84.js"
 );
 
 self.addEventListener('message', (event) => {
